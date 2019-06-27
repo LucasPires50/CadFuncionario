727b629f4d8175c99a4a11d296c61ea78da20e77
Lucas <luc_50@outlook.com>
2019-06-27 16:49:58 -0300

    Newcommit

---
+++ src/Modelo/Funcionario.java
@@ -0,0 +1,52 @@
+package Modelo;
+
+
+public class Funcionario {
+     //Com private não deixar o programador acessar todos as informção automaticamente, ele terar que aplicar o método get e set para pode aplicar essa informações
+    private String nome;
+    private double salario;
+    private int codCargo;
+    private int tempserviço;
+
+    //da retorno para acessar o valor
+    public String getNome() {
+        return this.nome;
+    }
+
+    //não da retorno da informação, só alterar o valor da variavel.
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return this.salario;
+
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getCodCargo() {
+        return this.codCargo;
+    }
+
+    public void setCodCargo(int codCargo) {
+        this.codCargo = codCargo;
+    }
+
+    public int gettempserviço() {
+        return this.tempserviço;
+    }
+
+    public void settempserviço(int tempserviço) {
+        this.tempserviço = tempserviço;
+    
+
+}
+}
+
+    
+
+
+
---
+++ src/cadfuncionario/CadFuncionarioPrincipal.java
@@ -0,0 +1,18 @@
+
+package cadfuncionario;
+
+import Modelo.Funcionario;
+
+public class CadFuncionarioPrincipal {
+
+    public static void main(String[] args) {
+         Funcionario f = new Funcionario();
+        
+        f.setNome("");
+        f.setSalario(0);
+        f.setCodCargo(0);
+        f.settempserviço(0);
+
+    }
+    
+}
